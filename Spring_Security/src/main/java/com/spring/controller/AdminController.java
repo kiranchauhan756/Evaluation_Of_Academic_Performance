9ca92581005cc39d8ee1d3467a873ebc179753c8
Kiran Chauhan <kc7563330@gmail.com>
2023-04-15 08:24:47 +0530

    admin functions

--- Spring_Security/src/main/java/com/spring/controller/AdminController.java
+++ Spring_Security/src/main/java/com/spring/controller/AdminController.java
@@ -5,9 +5,10 @@ import com.spring.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AdminController {
@@ -20,4 +21,36 @@ public class AdminController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
+    @PutMapping("/update/{email}")
+    public ResponseEntity<User> updateUser(@PathVariable String email,@RequestBody User user){
+        User user1=this.userService.updateUser(email,user);
+        if(user1==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+    }
+
+    @DeleteMapping("/delete/{email}")
+    public void deleteUser(@PathVariable String email){
+        this.userService.deleteUser(email);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<User>>getAll(){
+        List<User> list=new ArrayList<>();
+        list=this.userService.getAllUsers();
+        if(list.isEmpty())return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(list);
+    }
+    @GetMapping("/getUser/{email}")
+    public ResponseEntity<User> getSingleUSer(@PathVariable User user){
+
+        User user1=this.userService.getSingleUser(user);
+        if(user1==null)return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+    }
+
+
+
+
 }
--- Spring_Security/src/main/java/com/spring/service/UserServiceI.java
+++ Spring_Security/src/main/java/com/spring/service/UserServiceI.java
@@ -2,9 +2,18 @@ package com.spring.service;
 
 import com.spring.entities.User;
 
+import java.util.List;
 
 
 public interface UserServiceI {
     User addUser(User user);
     User login(User user);
+
+    User updateUser(String email, User user);
+
+    void deleteUser(String email);
+
+    List<User> getAllUsers();
+
+    User getSingleUser(User user);
 }
