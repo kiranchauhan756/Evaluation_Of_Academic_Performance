4136a85e38061f102c8d12c70903d2d95873feee
Kiran Chauhan <kc7563330@gmail.com>
2023-04-24 18:52:32 +0530

    mark exams

--- react_code/src/components/MarkInternals/MarkAssignment.js
+++ react_code/src/components/MarkInternals/MarkAssignment.js
@@ -1,13 +1,70 @@
 import AdminDash from "../AdminDashboard/AdminDash";
 import styled from "styled-components";
-//import { useRef, useState } from "react";
-import { React } from "react-router-dom";
+import { useState } from "react";
+import React, { useEffect, useParams, Link } from "react";
 
 const Assignment = () => {
+  const { email } = useParams();
+  const [userData, setUserdata] = useState([]);
+
+  useEffect(() => {
+    const request = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    };
+    fetch("http://localhost:8080/student/getAllSubjects/" + email, request)
+      .then((response) => response.json())
+      .then((detail) => {
+        setUserdata(detail);
+      });
+  }, []);
   return (
     <div>
       <AdminDash />
-      <Header></Header>
+      <Header>
+        {" "}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="d-grid d-md-flex justify-content-md-end mb-3">
+                <Link id="route" to={"/student"} className="button">
+                  Back
+                </Link>
+                <br />
+                <br />
+                <h5>subjects of student {email} are: </h5>
+                <br />
+                <table className="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th>Subject Code</th>
+                      <th>Subject Name</th>
+                      <th>Marks</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {userData.map((userData, index) => (
+                      <tr key={index}>
+                        <td>{userData.subjectCode} </td>
+                        <td>{userData.subjectName} </td>
+                        <td></td>
+
+                        <td>
+                          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                          <button type="button">Update</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Header>
     </div>
   );
 };
